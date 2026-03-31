2faa0a652bcbc319ca329fb531e964590b8fab8f
rominapro-crack <rominauwu892@gmail.com>
2026-03-31 20:44:32 -0300

    Se agrega marcador con popup

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var map = L.map('map').setView([-34.6037, -58.3816], 13);
-
+L.marker([-34.6037, -58.3816])
+   .addTo(map)
+   .bindPopup("Ubicación inicial")
+   .openPopup();
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: 'OpenStreetMap'
