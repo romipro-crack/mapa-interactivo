313ae28feb8b357a69272cdbcb1fa00d6811ad18
romipro-crack <rominauwu@gmail.com>
2026-04-01 21:29:19 -0300

    ej9- modo normal y oscuro

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 var map = L.map('map').setView([-34.6037, -58.3816], 13);
+
+// Modo claro
+var lightLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+});
+
+// Modo oscuro (CartoDB Dark)
+var darkLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    maxZoom: 19,
+    attribution: '© CartoDB © OpenStreetMap'
+});
+
+// agregar capa inicial
+lightLayer.addTo(map);
+
+// marcador
 L.marker([-34.6037, -58.3816])
    .addTo(map)
    .bindPopup("Ubicación inicial")
    .openPopup();
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   attribution: 'OpenStreetMap'
-}).addTo(map);
+// control de capas
+var capas = { 
+   "Mapa normal": lightLayer, 
+   "Mapa oscuro": darkLayer
+};
 
-L.marker([-34.6037, -58.3816])
-   .addTo(map)
-   .bindPopup("Ubicación inicial")
-   .openPopup();
-
-   let puntos = [];
-   map.on('click', function(e) {
-   puntos.push([e.latlng.lat, e.latlng.lng]);
-
-
-   if (puntos.length === 5) {
-       L.polyline(puntos, { color: 'blue' }).addTo(map);
-   }
-    });                   
-
+L.control.layers(capas).addTo(map);
